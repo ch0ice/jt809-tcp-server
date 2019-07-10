17d34811600f338e3d1be50db9ff36ae134936b8
Choice <mr_liuzeyuan@163.com>
2019-07-10 17:42:59 +0800

    修改配置文件读取

--- src/main/java/cn/com/onlinetool/jt809/config/BusinessConfig.java
+++ src/main/java/cn/com/onlinetool/jt809/config/BusinessConfig.java
@@ -3,7 +3,6 @@ package cn.com.onlinetool.jt809.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -22,24 +21,25 @@ public class BusinessConfig {
     /**
      * 下行消息缓存超时时间
      */
-    @Value("${downlink-msg-expire : 300}")
-    private Integer downlinkMsgExpire;
+    private Integer downlinkMsgExpire = 300;
 
     /**
      * 业务处理核心线程数
      */
-    @Value("${core-pool-size : 4}")
-    private Integer corePoolSize;
+    private Integer corePoolSize = 4;
 
     /**
      * 业务处理最大线程数
      */
-    @Value("${maximum-pool-size : 8}")
-    private Integer maximumPoolSize;
+    private Integer maximumPoolSize = 8;
 
     /**
      * 业务处理线程超时时间 秒
      */
-    @Value("${keep-alive-time : 10}")
-    private Integer keepAliveTime;
+    private Integer keepAliveTime = 10;
+
+    /**
+     * kafka输出topic
+     */
+    private String outputKafkaTopic;
 }
--- src/main/java/cn/com/onlinetool/jt809/config/NettyConfig.java
+++ src/main/java/cn/com/onlinetool/jt809/config/NettyConfig.java
@@ -3,7 +3,6 @@ package cn.com.onlinetool.jt809.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -23,36 +22,30 @@ public class NettyConfig {
     /**
      * netty server port
      */
-    @Value("${tcp-port : 11111}")
-    public Integer tcpPort;
+    public Integer tcpPort = 11111;
 
     /**
      * 主线程最大线程数
      */
-    @Value("${boss-max-thread-count : 4}")
-    public Integer bossMaxThreadCount;
+    public Integer bossMaxThreadCount = 4;
 
     /**
      * 工作线程最大线程数
      */
-    @Value("${work-max-thread-count : 8}")
-    public Integer workMaxThreadCount;
+    public Integer workMaxThreadCount = 8;
 
     /**
      * 数据包最大长度
      */
-    @Value("${max-frame-length : 65535}")
-    public Integer maxFrameLength;
+    public Integer maxFrameLength = 65535;
 
     /**
      * 单节点最大连接数
      */
-    @Value("${max-connect-num : 0}")
     public Integer maxConnectNum;
 
     /**
      * 客户端超时时间，如果空闲时间大于此值，关闭连接，单位秒
      */
-    @Value("${reader-idle-time-seconds : 60}")
-    public Integer readerIdleTimeSeconds;
+    public Integer readerIdleTimeSeconds = 60;
 }
