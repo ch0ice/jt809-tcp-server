ff958eea7371459697ecf47ccf377bdd596a6fd2
liuzy <liuzy@autoai.com>
2021-03-11 23:51:34 +0800

    增加下行消息发送 simple demo

--- src/test/java/cn/com/onlinetool/jt809/client/JT809TcpClientHandler.java
+++ src/test/java/cn/com/onlinetool/jt809/client/JT809TcpClientHandler.java
@@ -13,7 +13,6 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class JT809TcpClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
         System.out.println("channelActive..........");
         ctx.channel().write(ByteArrayUtil.hexStr2Bytes(
                 "5B000000480000005210010001E24001000100000000000001E2407465737438303900312E37312E3132392E32303100000000000000000000000000000000000000004E8ED9BA5D"
@@ -22,8 +21,8 @@ public class JT809TcpClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx,msg);
         System.out.println("channelRead.........." + ByteArrayUtil.bytes2HexStr((byte[])msg));
+        super.channelRead(ctx,msg);
     }
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
